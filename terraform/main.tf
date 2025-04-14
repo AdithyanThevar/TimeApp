ace25e1272de545059e5d26cff8c01fc8933c92d
Adithyan Thevar <atadi40@gmail.com>
2025-04-14 21:14:42 +0530

    changed port

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,7 +64,7 @@ resource "aws_ecs_service" "this" {
   load_balancer {
     target_group_arn = aws_lb_target_group.this.arn
     container_name   = "simpletime"
-    container_port   = 5000
+    container_port   = 9000
   }
 
   depends_on = [aws_lb_listener.this]
@@ -103,7 +103,7 @@ resource "aws_lb" "this" {
 
 resource "aws_lb_target_group" "this" {
   name     = "simpletime-tg"
-  port     = 5000
+  port     = 9000
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
   target_type = "ip"
@@ -152,8 +152,8 @@ resource "aws_security_group" "ecs" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port       = 5000
-    to_port         = 5000
+    from_port       = 9000
+    to_port         = 9000
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
