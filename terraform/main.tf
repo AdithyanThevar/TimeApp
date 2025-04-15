77705a0e5d122b72408ff96a523acda24f2c7380
Adithyan Thevar <atadi40@gmail.com>
2025-04-15 13:24:13 +0530

    terraform code changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.1.1"
 
-  name = "simpletime-vpc"
+  name = "timeApp-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["${var.aws_region}a", "${var.aws_region}b"]
@@ -17,16 +17,16 @@ module "vpc" {
   single_nat_gateway = true
 
   tags = {
-    Project = "SimpleTimeService"
+    Project = "TimeApp"
   }
 }
 
-resource "aws_ecs_cluster" "this" {
-  name = "simpletime-cluster"
+resource "aws_ecs_cluster" "timeapp_cluster" {
+  name = "TimeApp-cluster"
 }
 
-resource "aws_ecs_task_definition" "this" {
-  family                   = "simpletime-task"
+resource "aws_ecs_task_definition" "timeapp_task" {
+  family                   = "TimeApp-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -35,7 +35,7 @@ resource "aws_ecs_task_definition" "this" {
 
   container_definitions = jsonencode([
     {
-      name  = "simpletime"
+      name  = "TimeApp"
       image = var.image_url
       portMappings = [
         {
@@ -48,22 +48,22 @@ resource "aws_ecs_task_definition" "this" {
   ])
 }
 
-resource "aws_ecs_service" "this" {
-  name            = "simpletime-service"
-  cluster         = aws_ecs_cluster.this.id
+resource "aws_ecs_service" "timeapp_service" {
+  name            = "TimeApp-service"
+  cluster         = aws_ecs_cluster.timeapp_cluster.id
   launch_type     = "FARGATE"
   desired_count   = 1
-  task_definition = aws_ecs_task_definition.this.arn
+  task_definition = aws_ecs_task_definition.timeapp_task.arn
 
   network_configuration {
     subnets         = module.vpc.private_subnets
     assign_public_ip = false
-    security_groups  = [aws_security_group.ecs.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.this.arn
-    container_name   = "simpletime"
+    target_group_arn = aws_lb_target_group.timeapp_tg.arn
+    container_name   = "TimeApp"
     container_port   = 9000
   }
 
@@ -93,7 +93,7 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_lb" "this" {
+resource "aws_lb" "timeapp_alb" {
   name               = "simpletime-alb"
   internal           = false
   load_balancer_type = "application"
@@ -101,8 +101,8 @@ resource "aws_lb" "this" {
   security_groups    = [aws_security_group.alb.id]
 }
 
-resource "aws_lb_target_group" "this" {
-  name     = "simpletime-tg"
+resource "aws_lb_target_group" "timeapp_tg" {
+  name     = "TimeApp-tg"
   port     = 9000
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
@@ -117,18 +117,18 @@ resource "aws_lb_target_group" "this" {
   }
 }
 
-resource "aws_lb_listener" "this" {
-  load_balancer_arn = aws_lb.this.arn
+resource "aws_lb_listener" "timeapp_listener" {
+  load_balancer_arn = aws_lb.timeapp_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.this.arn
+    target_group_arn = aws_lb_target_group.timeapp_tg.arn
   }
 }
 
-resource "aws_security_group" "alb" {
+resource "aws_security_group" "alb_sg" {
   name   = "alb-sg"
   vpc_id = module.vpc.vpc_id
 
@@ -147,7 +147,7 @@ resource "aws_security_group" "alb" {
   }
 }
 
-resource "aws_security_group" "ecs" {
+resource "aws_security_group" "ecs_sg" {
   name   = "ecs-sg"
   vpc_id = module.vpc.vpc_id
 
